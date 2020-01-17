9c5f6971855e5ee59f794b7360d097a7e6bcba71
HolziHW <daholzhammer@tsn.at>
2020-01-17 17:21:37 +0100

    Bootstrap update auf Version 4, Navbar erweitert, weitere Controller erstellt

---
+++ Project4BHWII/Project4BHWII/Controllers/ParadoxController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project4BHWII.Controllers
+{
+    public class ParadoxController : Controller
+    {
+        // GET: Paradox
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
